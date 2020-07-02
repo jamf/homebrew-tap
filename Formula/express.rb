4ba6702f83dc947e10e369c5c35c2120fc774e42
Brian Stewart <brian.stewart@jamf.com>
2020-07-02 16:36:29 -0500

    Update metadata format for easier automatic updates

--- Formula/express.rb
+++ Formula/express.rb
@@ -2,13 +2,7 @@ class Express < Formula
   desc "A rapid application development toolset"
   homepage "https://github.com/jamf/express"
 
-  release_yaml = <<~YAML
-    version: "0.1.0"
-    url: "https://github.com/jamf/express/releases/download/0.1.0/express-darwin"
-    sha256: "5e77ac404f3d764dc7da734c383cd2548d3aa1a129a0d3c2d246b3bd91230b88"
-  YAML
-
-  release_metadata = YAML.load(release_yaml)
+  release_metadata = YAML.load(File.open(File.expand_path("../../metadata/express.yaml", __FILE__)).read)
 
   url release_metadata["url"]
   version release_metadata["version"]
